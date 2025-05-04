1bd820645032c455994409e87218eae2066e938d
Lubaba Nawla <lubabanawla@gmail.com>
2025-05-04 17:20:20 -0400

    Add files via upload
    Finished

--- ArrayResizer.java
+++ ArrayResizer.java
@@ -1,15 +1,53 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class ArrayResizer {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    /**
+     * Returns true if and only if every value in row r
+     * of array2D is non-zero.
+     * Precondition: r is a valid row index in array2D.
+     * Postcondition: array2D is unchanged.
+     */
+
+    public static boolean isNonZeroRow(int[][] array2D, int r) {
+        for(int i : array2D[r]) {
+            if(i==0) return false;
         }
+        return true;
+    }
+
+    /**
+     * Returns the number of rows in array2D that contain
+     * all non-zero values.
+     * Postcondition: array2D is unchanged.
+     */
+
+    public static int numNonZeroRows(int[][] array2D) {
+        int rows = 0;
+        for(int row = 0; row<array2D.length; row++) {
+            if(isNonZeroRow(array2D, row)) rows++;
+        }
+        return rows;
+    }
+
+    /**
+     * Returns a new, possibly smaller, two-dimensional array
+     * that contains only rows
+     * from array2D with no zeros, as described in part (b).
+     * Precondition: array2D contains at least one column and
+     * at least one row with no zeros.
+     * Postcondition: array2D is unchanged.
+     */
+
+    public static int[][] resize(int[][] array2D) {
+        int rows = numNonZeroRows(array2D);
+        int cols = array2D[0].length;
+        int[][] arr = new int[rows][cols];
+        int r = 0;
+        for(int row = 0; row<array2D.length; row++) {
+            if(isNonZeroRow(array2D, row)){
+                arr[r] = array2D[row];
+                r++;
+            }
+        }
+        return arr;
     }
 }
